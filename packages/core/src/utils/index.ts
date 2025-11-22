6fab47c20d3ab67052d4273c52ecf730dde853ce
Unknown-Robot <carondamien77@hotmail.fr>
2025-11-22 12:13:24 +0100

    Create the Utils class export

---
+++ packages/core/src/utils/index.ts
@@ -0,0 +1,1 @@
+export { default as Typeguard } from "./typeguard";
---
+++ packages/core/test/utils/index.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, test } from "@jest/globals";
+
+import * as Library from "../../src/utils";
+
+describe("Export Utils", () => {
+    test("Should export classes", () => {
+        expect(Library.Typeguard).toBeDefined();
+    });
+});
