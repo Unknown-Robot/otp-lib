0fe566b908690126571c37a44660ac884e0a0d56
Unknown-Robot <carondamien77@hotmail.fr>
2025-11-22 20:01:49 +0100

    Create the HOTPAuthenticator class

---
+++ packages/authenticator/src/hotp.ts
@@ -0,0 +1,142 @@
+import type { TAuthenticatorOptions } from "./types/authenticator";
+import type { IHOTPOptions } from "@otp-lib/core";
+
+import { AuthenticatorType } from "./enum/type";
+
+import Authenticator from "./authenticator";
+
+import { Typeguard } from "@otp-lib/core/utils";
+import { HOTP } from "@otp-lib/core";
+
+/**
+ * Class HOTPAuthenticator
+ * used to implements the HMAC-Based One-Time Password Authenticator
+ *
+ * @class HOTPAuthenticator
+ * @extends {Authenticator<HOTP, IHOTPOptions>}
+ */
+class HOTPAuthenticator extends Authenticator<HOTP, IHOTPOptions> {
+    /**
+     * Constructor of HOTPAuthenticator class
+     * 
+     * @param {TAuthenticatorOptions<IHOTPOptions>} options The options of HOTP
+     * @memberof HOTPAuthenticator
+     */
+    constructor(options: TAuthenticatorOptions<IHOTPOptions>) {
+        super(new HOTP(options), options);
+    }
+
+    /**
+     * Set the counter value of HOTP authenticator
+     *
+     * @param {number} counter The counter value of HOTP authenticator
+     * @memberof HOTPAuthenticator
+     */
+    public setCounter(counter: number): void {
+        return this.otp.setCounter(counter);
+    }
+
+    /**
+     * Get the look-ahead window of HOTP authenticator
+     *
+     * @return {number}
+     * @memberof HOTPAuthenticator
+     */
+    public getLookAhead(): number {
+        return this.otp.getLookAhead();
+    }
+
+    /**
+     * Get the counter value of HOTP authenticator
+     *
+     * @return {number}
+     * @memberof HOTPAuthenticator
+     */
+    public getCounter(): number {
+        return this.otp.getCounter();
+    }
+
+    /**
+     * Generate the HOTP code from current counter value
+     *
+     * @return {Promise<string>}
+     * @memberof HOTPAuthenticator
+     */
+    public generate(): Promise<string> {
+        return this.otp.generate();
+    }
+
+    /**
+     * Verify the HOTP code delta for the current counter value
+     *
+     * @param {string} code The HOTP code to verify
+     * @return {Promise<number | null>}
+     * @memberof HOTPAuthenticator
+     */
+    public verifyDelta(code: string): Promise<number | null> {
+        return this.otp.verifyDelta(code);
+    }
+
+    /**
+     * Verify if the HOTP code is valid for the current counter value
+     *
+     * @param {string} code The HOTP code to verify
+     * @return {Promise<boolean>}
+     * @memberof HOTPAuthenticator
+     */
+    public async verify(code: string): Promise<boolean> {
+        return this.otp.verify(code);
+    }
+
+    /**
+     * Get the HOTP authenticator from URI
+     *
+     * @static
+     * @param {string} uri The HOTP URI to parse 
+     * @return {HOTPAuthenticator}
+     * @memberof HOTPAuthenticator
+     */
+    public static fromURI(uri: string): HOTPAuthenticator {
+        const url = new URL(uri);
+
+        if(url.host !== AuthenticatorType.HOTP) {
+            throw(new Error("The authenticator URI type is invalid"));
+        }
+
+        const options: TAuthenticatorOptions<IHOTPOptions> = super.fromURL(url);
+
+        const counter = url.searchParams.get("counter");
+        if(counter !== null) {
+            const value = parseInt(counter, 10);
+            if(!Typeguard.isPositiveInteger(value)) {
+                throw(new Error("The authenticator URI counter is invalid"));
+            }
+            
+            options.counter = value;
+        }
+        else {
+            throw(new Error("The authenticator URI counter is required"));
+        }
+
+        return new this(options);
+    }
+
+    /**
+     * Get the URI of HOTP authenticator
+     *
+     * @return {string}
+     * @memberof HOTPAuthenticator
+     */
+    public toURI(): string {
+        const url = super.toURL();
+
+        url.host = AuthenticatorType.HOTP;
+
+        const counter = this.getCounter();
+        url.searchParams.set("counter", counter.toString());
+
+        return url.toString();
+    }
+}
+
+export default HOTPAuthenticator;
---
+++ packages/authenticator/test/hotp.test.ts
@@ -0,0 +1,156 @@
+import { jest, describe, expect, test, afterEach } from "@jest/globals";
+
+import type { TAuthenticatorOptions } from "../src/types/authenticator";
+import type { IHOTPOptions } from "@otp-lib/core";
+
+import { AuthenticatorType } from "../src/enum/type";
+
+import HOTPAuthenticator from "../src/hotp";
+import { HOTP } from "@otp-lib/core";
+
+describe("HMAC-Based One-Time Password Authenticator", () => {
+    describe("Constructor", () => {
+        test("Should use default option values", () => {
+            const options: TAuthenticatorOptions<IHOTPOptions> = {
+                account: "account@test.com"
+            };
+
+            const hotp = new HOTPAuthenticator(options);
+
+            expect(hotp.getAccount()).toBe(options.account);
+            expect(hotp.getIssuer()).toBeNull();
+        });
+
+        test("Should use defined option values", () => {
+            const options: TAuthenticatorOptions<IHOTPOptions> = {
+                account: "account@test.com",
+                issuer: "Issuer"
+            };
+
+            const hotp = new HOTPAuthenticator(options);
+
+            expect(hotp.getAccount()).toBe(options.account);
+            expect(hotp.getIssuer()).toBe(options.issuer);
+        });
+    });
+
+    describe("Proxy methods", () => {
+        const options: TAuthenticatorOptions<IHOTPOptions> = {
+            account: "account"
+        };
+
+        options.lookAhead = 5;
+        options.counter = 3;
+
+        const authenticator = new HOTPAuthenticator(options);
+
+        afterEach(() => {
+            jest.restoreAllMocks();
+        });
+
+        test("Should call proxy method setCounter", () => {
+            const spy = jest.spyOn(HOTP.prototype, "setCounter");
+            const authenticator = new HOTPAuthenticator(options);
+
+            authenticator.setCounter(42);
+            expect(spy).toHaveBeenCalledWith(42);
+        });
+
+        test("Should call proxy method getLookAhead", () => {
+            const spy = jest.spyOn(HOTP.prototype, "getLookAhead");
+
+            expect(authenticator.getLookAhead()).toBe(options.lookAhead);
+            expect(spy).toHaveBeenCalledTimes(1);
+        });
+
+        test("Should call proxy method getCounter", () => {
+            const spy = jest.spyOn(HOTP.prototype, "getCounter");
+
+            expect(authenticator.getCounter()).toBe(options.counter);
+            expect(spy).toHaveBeenCalledTimes(1);
+        });
+
+        test("Should call proxy method generate", async() => {
+            const spy = jest.spyOn(HOTP.prototype, "generate");
+
+            await authenticator.generate();
+
+            expect(spy).toHaveBeenCalledTimes(1);
+        });
+
+        test("Should call proxy method verifyDelta", async() => {
+            const spy = jest.spyOn(HOTP.prototype, "verifyDelta");
+
+            const code = await authenticator.generate();
+            const delta = await authenticator.verifyDelta(code);
+
+            expect(spy).toHaveBeenCalledWith(code);
+            expect(delta).toBe(0);
+        });
+
+        test("Should call proxy method verify", async() => {
+            const spy = jest.spyOn(HOTP.prototype, "verify");
+
+            const code = await authenticator.generate();
+            const verify = await authenticator.verify(code);
+
+            expect(spy).toHaveBeenCalledWith(code);
+            expect(verify).toBe(true);
+        });
+    });
+
+    describe("HOTP Authenticator URI", () => {
+        test("Should return default HOTP Authenticator URI", () => {
+            const options: TAuthenticatorOptions<IHOTPOptions> = {
+                account: "account@test.com"
+            };
+
+            const authenticator = new HOTPAuthenticator(options);
+            const uri = new URL(authenticator.toURI());
+            
+            expect(uri.host).toBe(AuthenticatorType.HOTP);
+            expect(uri.searchParams.get("counter")).toBe("0");
+        });
+
+        test("Should return defined HOTP Authenticator URI", () => {
+            const options: TAuthenticatorOptions<IHOTPOptions> = {
+                account: "account@test.com"
+            };
+
+            options.counter = 451;
+
+            const authenticator = new HOTPAuthenticator(options);
+            const uri = new URL(authenticator.toURI());
+            
+            expect(uri.searchParams.get("counter")).toBe(options.counter.toString());
+        });
+
+        test("Should throw invalid type from HOTP Authenticator URI", () => {
+            expect(() => HOTPAuthenticator.fromURI("otpauth://totp"))
+                .toThrow(`The authenticator URI type is invalid`);
+        });
+
+        test("Should throw required counter from HOTP Authenticator URI", () => {
+            expect(() => HOTPAuthenticator.fromURI("otpauth://hotp/account?secret=234567"))
+                .toThrow(`The authenticator URI counter is required`);
+        });
+
+        describe("Should throw invalid counter from HOTP Authenticator URI", () => {
+            const invalidCounter = [
+                "otpauth://hotp/account?secret=234567&counter=",
+                "otpauth://hotp/account?secret=234567&counter=abc"
+            ];
+
+            test.each(invalidCounter)(`Should throw invalid counter "%s"`, (uri) => {
+                expect(() => HOTPAuthenticator.fromURI(uri))
+                    .toThrow(`The authenticator URI counter is invalid`);
+            });
+        });
+
+        test("Should return counter from HOTP Authenticator URI", () => {
+            const authenticator = HOTPAuthenticator.fromURI("otpauth://hotp/account?secret=234567&counter=30");
+
+            expect(authenticator.getCounter()).toBe(30);
+        });
+    });
+});
