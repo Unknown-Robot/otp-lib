9363534b27a009cb5116ac4e64ff6025250f45df
Unknown-Robot <carondamien77@hotmail.fr>
2025-11-22 12:14:37 +0100

    Create the core package export

---
+++ packages/core/src/index.ts
@@ -0,0 +1,10 @@
+export { default as OTP } from "./otp";
+export { default as HOTP } from "./hotp";
+export { default as TOTP } from "./totp";
+export { default as Secret } from "./secret";
+
+export * from "./enum/hash";
+
+export type * from "./types/otp";
+export type * from "./types/hotp";
+export type * from "./types/totp";
---
+++ packages/core/test/index.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, test } from "@jest/globals";
+
+import * as Library from "../src/index";
+
+describe("Entry Point", () => {
+    test("Should export classes", () => {
+        expect(Library.OTP).toBeDefined();
+        expect(Library.HOTP).toBeDefined();
+        expect(Library.TOTP).toBeDefined();
+        expect(Library.Secret).toBeDefined();
+    });
+
+    test("Should export enum", () => {
+        expect(Library.HashAlgorithms).toBeDefined();
+    });
+});
